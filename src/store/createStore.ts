35bdc3fcb9d00312dc2316b5583ed199b44d221a
caili <li.cai@jfjun.com>
2018-07-19 15:58:42 +0800

    home： 样式修改

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -6,23 +6,27 @@ import rootReducer from 'reducers/index'
 import { StoreState } from './storeState'
 
 export function configureStore(initialState?: StoreState): Store<StoreState> {
-  const middlewares: Middleware[] = [
-      thunkMiddleware,
-      createLogger(),
-  ]
+    // thunkMiddleware:允许使用 dispatch;createLogger:打印action日志
+    const middlewares: Middleware[] = [
+        thunkMiddleware,
+        createLogger(),
+    ]
 
-  const composeEnhancers =
-      DEBUG && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+    const composeEnhancers =
+        DEBUG && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
 
-  const store = createStore(rootReducer, initialState, composeEnhancers(
-      applyMiddleware(...middlewares),
-  ))
+    const store = createStore(
+        rootReducer,
+        initialState,
+        composeEnhancers(
+            applyMiddleware(...middlewares),
+        ))
 
-  if (module.hot) {
-      module.hot.accept('./../reducers/index', () => {
-          store.replaceReducer((require('./../reducers/index') as Reducer<StoreState>))
-      })
-  }
+    if (module.hot) {
+        module.hot.accept('./../reducers/index', () => {
+            store.replaceReducer((require('./../reducers/index') as Reducer<StoreState>))
+        })
+    }
 
-  return store
+    return store
 }
